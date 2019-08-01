82847bc26252e23d6e7a12f07c4781051636a50b
Pawel <pgrzeszczak@ethernet.pl>
2019-08-01 07:22:36 +0200

    update wyjatki

--- kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/io/FileReader.java
@@ -10,7 +10,7 @@ import java.util.stream.Stream;
 
 public class FileReader {
 
-    public void readFile() {
+    public void readFile() throws FileReaderException {
         ClassLoader classLoader = getClass().getClassLoader();
         File file = new File(classLoader.getResource("file/names.txt").getFile());
         //Path path = Paths.get(file.getPath());
@@ -22,7 +22,9 @@ public class FileReader {
 
         } catch (IOException e) {
 
-            System.out.println("Oh no! Something went wrong! Error:" + e);
+            throw new FileReaderException();
+
+            //System.out.println("Oh no! Something went wrong! Error:" + e);
 
         } finally {
 
--- kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
+++ kodilla-exception/src/main/java/com/kodilla/exception/main/ExceptionModuleRunner.java
@@ -1,12 +1,32 @@
 package com.kodilla.exception.main;
 
 import com.kodilla.exception.io.FileReader;
+import com.kodilla.exception.io.FileReaderException;
+import com.kodilla.exception.io.FileReaderWithoutHandling;
+import com.kodilla.exception.test.SecondChallenge;
 
 public class ExceptionModuleRunner {
 
     public static void main(String[] args) {
 
+//        FileReader fileReader = new FileReader();
+//        fileReader.readFile();
+
+//        FileReaderWithoutHandling fileReader = new FileReaderWithoutHandling();
+//        fileReader.readFile();
+
         FileReader fileReader = new FileReader();
-        fileReader.readFile();
+
+        try {
+            fileReader.readFile();
+        } catch (FileReaderException e) {
+            System.out.println("Problem while reading a file!");
+        }
+
+        SecondChallenge secondChallenge = new SecondChallenge();
+
+        String s = secondChallenge.probablyIWillThrowException(2, 1);
+        System.out.println("Result: " + s);
+
     }
 }
