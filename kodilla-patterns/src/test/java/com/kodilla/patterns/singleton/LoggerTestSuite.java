33f30b9c140dd51fa16632e4639b6f56ecbe1240
Pawel <pgrzeszczak@ethernet.pl>
2019-10-20 15:51:47 +0200

    Task 15.1 testing method getLastLog() class Logger

---
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/singleton/LoggerTestSuite.java
@@ -0,0 +1,37 @@
+package com.kodilla.patterns.singleton;
+
+import org.junit.Assert;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class LoggerTestSuite {
+    private static Logger logger;
+
+    @BeforeClass
+    public static void createLog() {
+        logger = new Logger();
+        logger.log("logs logs logs");
+    }
+
+    @Test
+    public void getLastLog() {
+        //Given
+        //When
+        String logName = logger.getLastLog();
+        System.out.println("Last log: " + logName);
+        //Then
+        Assert.assertEquals("logs logs logs", logName);
+    }
+}
+
+
+
+
+
+
+//   1. Utwórz w katalogu src/test/java w pakiecie com.kodilla.patterns.singleton nowy zestaw testów o nazwie LoggerTestSuite,
+//    a w nim testy sprawdzające działanie metody getLastLog() klasy Logger. Aby to przetestować,
+//    zapisz coś wcześniej do logów przy pomocy metody log(String log).
+//        2. Zmodyfikuj kod klasy Logger tak, aby była zgodna ze wzorcem Singleton.
+//        3. Zmodyfikuj napisane przed chwilą testy tak, aby uwzględniały zmiany w klasie Logger.
+//        4. Utworzony kod prześlij do repozytorium GitHub.
