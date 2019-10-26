389338f4c90e1b43f5198f6399ac133f173a645d
Pawel <pgrzeszczak@ethernet.pl>
2019-10-26 14:21:54 +0200

    Patterns examples material 15.2

--- kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/BoardTestSuite.java
+++ kodilla-patterns/src/test/java/com/kodilla/patterns/prototype/BoardTestSuite.java
@@ -31,10 +31,36 @@ public class BoardTestSuite {
         board.getLists().add(listToDo);
         board.getLists().add(listInProgress);
         board.getLists().add(listDone);
-        System.out.println(board);
+
+        //making a shallow copy of object board
+        Board clonedBoard = null;
+        try {
+            clonedBoard = board.shallowCopy();
+            clonedBoard.setName("Project number 2");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
+
+        //making a deep copy of object board
+        Board deepClonedBoard = null;
+        try {
+            deepClonedBoard = board.deepCopy();
+            deepClonedBoard.setName("Project number 3");
+        } catch (CloneNotSupportedException e) {
+            System.out.println(e);
+        }
 
         //When
+        board.getLists().remove(listToDo);
+
         //Then
-        Assert.assertEquals(3, board.getLists().size());
+        System.out.println(board);
+        System.out.println(clonedBoard);
+        System.out.println(deepClonedBoard);
+        Assert.assertEquals(2, board.getLists().size());
+        Assert.assertEquals(2, clonedBoard.getLists().size());
+        Assert.assertEquals(3, deepClonedBoard.getLists().size());
+        Assert.assertEquals(clonedBoard.getLists(), board.getLists());
+        Assert.assertNotEquals(deepClonedBoard.getLists(), board.getLists());
     }
 }
