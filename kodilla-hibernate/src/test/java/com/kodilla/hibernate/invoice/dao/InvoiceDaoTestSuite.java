5fd611facb7eb67f2d14b6ca22b26db32727e4cc
Pawel <pgrzeszczak@ethernet.pl>
2019-11-26 15:54:56 +0100

    Task 17.3

---
+++ kodilla-hibernate/src/test/java/com/kodilla/hibernate/invoice/dao/InvoiceDaoTestSuite.java
@@ -0,0 +1,59 @@
+package com.kodilla.hibernate.invoice.dao;
+
+import com.kodilla.hibernate.invoice.Invoice;
+import com.kodilla.hibernate.invoice.Item;
+import com.kodilla.hibernate.invoice.Product;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import java.math.BigDecimal;
+import java.util.Arrays;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class InvoiceDaoTestSuite {
+    @Autowired
+    ProductDao productDao;
+    @Autowired
+    ItemDao itemDao;
+    @Autowired
+    InvoiceDao invoiceDao;
+
+    @Test
+    public void testInvoiceDaoSave() {
+        //Given
+        Product product1 = new Product("TV");
+        Product product2 = new Product("iPhone");
+        Product product3 = new Product("MacBook");
+
+        Item item1 = new Item(product1, new BigDecimal("7600"), 2);
+        Item item2 = new Item(product2, new BigDecimal("5000"), 3);
+        Item item3 = new Item(product3, new BigDecimal("16700"), 1);
+
+        Invoice invoice1 = new Invoice("Faktura1/11/2019");
+        invoice1.setItems(Arrays.asList(item1, item2, item3));
+
+        //When
+        invoiceDao.save(invoice1);
+        int invoice1Id = invoice1.getId();
+
+        //Then
+        Assert.assertEquals(3, productDao.count());
+        Assert.assertEquals(3, itemDao.count());
+        Assert.assertEquals(1, invoiceDao.count());
+        Assert.assertNotEquals(0, invoice1Id);
+
+        //CleanUp
+/*        try {
+            productDao.deleteAll();
+            itemDao.deleteAll();
+            invoiceDao.deleteAll();
+        } catch (Exception e) {
+            // do nothing
+        }*/
+    }
+}
