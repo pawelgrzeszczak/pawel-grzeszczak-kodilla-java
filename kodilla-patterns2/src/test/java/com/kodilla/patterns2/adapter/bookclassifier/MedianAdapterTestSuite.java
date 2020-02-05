e8c82f671cf60cc9c32aeabac8090cc3b61287fd
Pawel <pgrzeszczak@ethernet.pl>
2020-02-05 08:21:50 +0100

    Task 24.5 FIX

--- kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
+++ kodilla-patterns2/src/test/java/com/kodilla/patterns2/adapter/bookclassifier/MedianAdapterTestSuite.java
@@ -1,6 +1,5 @@
 package com.kodilla.patterns2.adapter.bookclassifier;
 
-import com.kodilla.patterns2.adapter.bookclassifier.librarya.Book;
 import org.junit.Test;
 
 import java.util.Arrays;
@@ -13,10 +12,10 @@ public class MedianAdapterTestSuite {
     @Test
     public void publicationYearMedian() {
         //given
-        Book book1 = new Book("John", "Smith", 2008, "24125322");
-        Book book2 = new Book("John1", "Smith2", 2002, "31232113");
-        Book book3 = new Book("John3", "Smith3", 2010, "23131345");
-        Set<Book> books = new HashSet<>(Arrays.asList(book1, book2, book3));
+        com.kodilla.patterns2.adapter.bookclassifier.librarya.Book book1 = new com.kodilla.patterns2.adapter.bookclassifier.librarya.Book("John", "Smith", 2008, "24125322");
+        com.kodilla.patterns2.adapter.bookclassifier.librarya.Book book2 = new com.kodilla.patterns2.adapter.bookclassifier.librarya.Book("John1", "Smith2", 2002, "31232113");
+        com.kodilla.patterns2.adapter.bookclassifier.librarya.Book book3 = new com.kodilla.patterns2.adapter.bookclassifier.librarya.Book("John3", "Smith3", 2010, "23131345");
+        Set<com.kodilla.patterns2.adapter.bookclassifier.librarya.Book> books = new HashSet<>(Arrays.asList(book1, book2, book3));
         MedianAdapter medianAdapter = new MedianAdapter();
         //when
         int median = medianAdapter.publicationYearMedian(books);
